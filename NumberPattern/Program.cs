199e5b6e0205a0fafb82ba6d01ced91edeb5f6f1
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2022-12-05 15:05:43 -0800

    Add project files.

---
+++ NumberPattern/Program.cs
@@ -0,0 +1,173 @@
+﻿namespace NumberPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Example1();
+            //Example2();
+            //Example3();
+            //Example4();
+            //Example5();
+            //Example6();
+            //Example7();
+            //Example9();
+            //Example10();
+            Example11();
+        }
+
+        public static void Example1()
+        {
+            int x, y;
+            for (x = 1; x <= 5; x++)
+            {
+                for (y = 1; y <= x; y++)
+                {
+                    Console.Write(y);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+        public static void Example2()
+        {
+            int x, y;
+            for (x = 5; x >= 1; x--)
+            {
+                for (y = 1; y <= x; y++)
+                {
+                    Console.Write(y);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+        public static void Example3()
+        {
+            int x, y;
+            for (x = 5; x >= 1; x--)
+            {
+                for (y = x; y <= 5; y++)
+                {
+                    Console.Write(y);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+        public static void Example4()
+        {
+            int x, y;
+            for (x = 1; x <= 5; x++)
+            {
+                for (y = x; y <= 5; y++)
+                {
+                    Console.Write(y);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+        public static void Example5()
+        {
+            int x, y;
+            for (x = 1; x <= 5; x++)
+            {
+                for (y = 1; y <= x; y++)
+                {
+                    Console.Write(x);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+        public static void Example6()
+        {
+            int x, y;
+            for (x = 5; x >= 1; x--)
+            {
+                for (y = 5; y >= x; y--)
+                {
+                    Console.Write(x);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+        public static void Example7()
+        {
+            int x, y;
+            for (x = 5; x >= 1; x--)
+            {
+                for (y = 1; y <= x; y++)
+                {
+                    Console.Write(x);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+        public static void Example8()
+        {
+            int x, y;
+            for (x = 1; x <= 5; x++)
+            {
+                for (y = 5; y >= x; y--)
+                {
+                    Console.Write(x);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+        public static void Example9()
+        {
+            int x, y;
+            for (x = 6; x >= 1; x--)
+            {
+                for (y = x; y >= 1; y--)
+                {
+                    Console.Write(y);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+        public static void Example10()
+        {
+            int x, y;
+            for (x = 1; x <= 5; x++)
+            {
+                for (y = 6; y >= x; y--)
+                {
+                    Console.Write(y);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+        public static void Example11()
+        {
+            int x, y;
+            for (x = 7; x >= 1; x -= 2)
+            {
+                for (y = 1; y <= x; y++)
+                {
+                    Console.Write(y);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NumberPattern/README.md
@@ -0,0 +1,3 @@
+# Number Pattern
+This application serves as a way to see how nested loops can generate numbers in sequential order.
+Uncomment out any of the examples to see the desired result. 
